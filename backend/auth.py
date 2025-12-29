c2d7508ce9c93a2b149c48266ccefcbc4c2f6ad6
Ananjay07 <ananjay1357@gmail.com>
2025-12-29 12:02:35 +0530

    Remove unused sqlalchemy import

--- backend/auth.py
+++ backend/auth.py
@@ -4,7 +4,7 @@ from jose import JWTError, jwt
 from passlib.context import CryptContext
 from fastapi import Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer
-from sqlalchemy.orm import Session
+
 from database import get_db
 from models import User
 import os
